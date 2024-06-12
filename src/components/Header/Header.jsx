874215922467e8d9969782c8835229993851ba3e
Stephanie Nyambuya <tsitsinyambuya73@gmail.com>
2024-06-12 11:35:51 -0700

    add changes to files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./Header.scss";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="header">
+      Header
+      <div className="header__player1">
+        <div className="header">
+          <h1>Player 1:</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
+import Header from "../Header/Header";
 
 function main() {
-    function handleClick(id) {
-        console.log("I have been clicked");
-    }
+  function handleClick(id) {
+    console.log("I have been clicked");
+  }
   return (
     <>
-        <div className="main">
-            <form className="main__big-wrapper" onClick={handleClick}>
-                <div className="main__little-wrapper">
-                    <div className="main__buttons" id='box1'></div>
-                </div>
-            </form>
-        </div>
+      <Header />
+      <div className="main">
+        <form className="main__big-wrapper" onClick={handleClick}>
+          <div className="main__little-wrapper">
+            <div className="main__buttons" id="box1"></div>
+          </div>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default main
+export default main;
