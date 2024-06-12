67609e06b60b11b5eb07fa14df31e04b1543e9f5
Stephanie Nyambuya <tsitsinyambuya73@gmail.com>
2024-06-12 13:29:48 -0700

    add modifications to files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <Main /> */}
+      <Main />
       <Footer />
     </>
   );
--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,29 +1,58 @@
 import React from "react";
+import "./Main.scss";
 import { useState } from "react";
 
-function main() {
-  const [gameScores, setGameScores] = useState([]);
+function Main() {
+  const [tictacArr, setTictacArr] = useState([
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+  ]);
+  const [playerTurn, setPlayerTurn] = useState(true);
 
-  useEffect(() => {
-    const getGameData = async () => {
-      const response = await axios.get();
-    };
-  });
-
-  function handleClick(id) {
-    console.log("I have been clicked");
+  function clickHandler(e) {
+    e.preventDefault();
+    console.log(e.target.id);
+    tictacArr[e.target.id] = playerTurn;
+    setPlayerTurn(!playerTurn);
+    const temp = [...tictacArr];
+    setTictacArr(temp);
   }
+
+  function renderGrid() {
+    const gridItems = [];
+    for (let i = 0; i < 9; i++) {
+      gridItems.push(
+        <div key={i} className="main__little-wrapper">
+          {/* {console.log(tictacArr[i])} */}
+          {tictacArr[i] !== " " ? (
+            <div className="main__buttons" id={`${i}`}>
+              {tictacArr[i] === true ? "X" : "O"}
+            </div>
+          ) : (
+            <div className="main__buttons" id={`${i}`}></div>
+          )}
+        </div>
+      );
+    }
+    return gridItems;
+  }
+
   return (
     <>
       <div className="main">
-        <form className="main__big-wrapper" onClick={handleClick}>
-          <div className="main__little-wrapper">
-            <div className="main__buttons" id="box1"></div>
-          </div>
+        <form className="main__big-wrapper" onClick={clickHandler}>
+          {renderGrid()}
         </form>
       </div>
     </>
   );
 }
 
-export default main;
+export default Main;
